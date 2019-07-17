3adc2d3c28dd551df2acef4779c3a867470f01eb
robertburrowes <r.burrowes@auckland.ac.nz>
2019-07-18 09:59:13 +1200

    trying different calls

--- Python/test_infoblox.py
+++ Python/test_infoblox.py
@@ -12,8 +12,8 @@ with open( conf_file ) as f:
 iba_api = infoblox.Infoblox('ipam.auckland.ac.nz', conf['user'], conf['password'], '2.5', 'default', 'default', True)
 print "**** By IP ****"
 try:
-    iba_api.create_host_record('130.216.218.66', 'mytardis.nectar.auckland.ac.nz') #Attach a name to an IP address
-    hosts = iba_api.get_host_by_ip('130.216.218.66') #Check that the name is attached.
+    #iba_api.create_host_record('130.216.218.66', 'mytardis.nectar.auckland.ac.nz') #Attach a name to an IP address
+    hosts = iba_api.get_host_by_ip('130.216.216.45') #Check that the name is attached.
     print hosts
 except Exception as e:
     print e
@@ -24,3 +24,26 @@ try:
   print hosts
 except Exception as e:
   print e
+
+print "**** host by regular expression ****"
+try:
+  hosts = iba_api.get_host_by_regexp(fqdn='.*ceres.*')
+  print hosts
+except Exception as e:
+  print e
+
+print "**** get network *****"
+try:
+    hosts = iba_api.get_network(network='130.216.161.0/24')
+    print hosts
+except Exception as e:
+  print e
+
+
+#print "**** deleting host record ****"
+#try:
+#    iba_api.delete_host_record(fqdn='dashboard.uoa.nesi.org.nz')
+#except Exception as e:
+#    print e
+
+print "**** Done ****"
